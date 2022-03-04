6cfaba71f90cc41a851a0513d750969692c9766f
Phạm Tiến Dũng <56197293+phamtiendungcwit@users.noreply.github.com>
2022-03-04 11:24:12 +0700

    Add Meta Part 1

---
+++ DTO/MetaDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class MetaDTO
+    {
+        public int MetaID { get; set; }
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Hãy nhập nội dụng khu vực")]
+        public string MetaContent { get; set; }
+    }
+}
---
+++ UI/Areas/Admin/Controllers/MetaController.cs
@@ -0,0 +1,24 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Areas.Admin.Controllers
+{
+    public class MetaController : Controller
+    {
+        // GET: Admin/Meta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddMeta()
+        {
+            MetaDTO dto = new MetaDTO();
+            return View(dto);
+        }
+    }
+}
