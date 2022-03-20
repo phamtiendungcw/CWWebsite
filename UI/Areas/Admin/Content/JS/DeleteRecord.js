493c3f2f4604d2336b002231705065f1e2de3dec
Phạm Tiến Dũng <56197293+phamtiendungcwit@users.noreply.github.com>
2022-03-20 15:34:41 +0700

    Add Modal and Delete Record Script

---
+++ UI/Areas/Admin/Content/JS/DeleteRecord.js
@@ -0,0 +1,17 @@
+﻿var URL = "";
+var ID = "";
+function AskQuestion(url, id) {
+    $("#modalmessage").modal();
+    URL = url;
+    ID = id;
+}
+function Delete() {
+    $.ajax({
+        url: URL + ID,
+        type: "POST",
+        success: function (result) {
+            $("#a_" + ID).fadeOut();
+            $("#modalmessage").modal('hide');
+        }
+    })
+}
